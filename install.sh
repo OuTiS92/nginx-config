a77dbec7af7b3ee2917f9ff952436d94ffb3b5b1
outis <amirhoseinalahdad76@gmail.com>
2023-09-09 19:48:32 +0330

    add file install to nginx config

---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo "install and confing nginx manually:"
+sleep 2 
+clear
+echo "wait ..... "
+sudo apt update 
+wait
+apt install pcre-devel 
+wait
+apt-get install libpcre3 libpcre3-dev
+wait
+apt-get install zlib1g-dev
+wait
+apt install libgd-dev
+wait
+apt install libgd-dev
+wait
+clear
+
+echo "soon install config nginx ...."
+sleep 2
+
+
+wget http://nginx.org/download/nginx-1.25.2.tar.gz
+wait
+tar -zxvf nginx-1.25*
+wait
+cd nginx-1.25*
+sleep 1
+clear
+
+./configure --sbin-path=/usr/bin/nginx --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log  --pid-path=/usr/run/nginx.pid --with-http_ssl_module --with-http_image_filter_module=dynamic --modules-path=/etc/nginx/modules  --without-http_rewrite_module 
+wait 
+
+echo " insall Ok :) "
+sleep 3 
+clear
+
+systemctl status nginx
+
+
