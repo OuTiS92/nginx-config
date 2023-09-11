2fe60f736fdeef1af03e22063771215a0502e164
OuTiS <33751897+OuTiS92@users.noreply.github.com>
2023-09-12 00:56:51 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -53,12 +53,25 @@ wait
 systemctl enable nginx
 sleep 2
 systemctl start nginx
-clear; for i in {0..10000..33} 10000;do i=0$i
-     printf -v p %0.2f ${i::-2}.${i: -2}
-     percentBar $p $((COLUMNS-7)) bar
-     printf '\r\e[47;30m%s\e[0m%6.2f%%' "$bar" $p
-     read -srt .002 _ && break
+percentBar ()  {
+    local prct totlen=$((8*$2)) lastchar barstring blankstring;
+    printf -v prct %.2f "$1"
+    ((prct=10#${prct/.}*totlen/10000, prct%8)) &&
+        printf -v lastchar '\\U258%X' $(( 16 - prct%8 )) ||
+            lastchar=''
+    printf -v barstring '%*s' $((prct/8)) ''
+    printf -v barstring '%b' "${barstring// /\\U2588}$lastchar"
+    printf -v blankstring '%*s' $(((totlen-prct)/8)) ''
+    printf -v "$3" '%s%s' "$barstring" "$blankstring"
+}
+clear
+for i in {0..10000..33} 10000;do i=0$i
+    printf -v p %0.2f ${i::-2}.${i: -2}
+    percentBar $p $((COLUMNS-15)) bar
+    printf '\r|%s|%6.2f%%' "$bar" $p
+    read -srt .002 _ && break    # console sleep avoiding fork
 done
-
+        echo "\n"
+    echo "Install Done"
 
 
