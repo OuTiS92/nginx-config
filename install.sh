8de46808970b46dea26fc228cf55e7eef31a0f6c
OuTiS <33751897+OuTiS92@users.noreply.github.com>
2023-09-29 19:46:57 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ clear
 ./configure --sbin-path=/usr/bin/nginx --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --with-pcre --pid-path=/var/run/nginx.pid --with-http_ssl_module --with-http_image_filter_module=dynamic --modules-path=/etc/nginx/modules  --without-http_proxy_module --without-http_rewrite_module --without-http_rewrite_module 
 wait 
 sleep 3 
-clear
+clear 
 make 
 wait 
 make install
